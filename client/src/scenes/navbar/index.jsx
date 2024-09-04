49986dadc598725f50afe3dfe3dbbc69b1fe301b
yash <yash03saini@gmail.com>
2024-09-04 18:54:11 +0530

    first commit

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          Connecto
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
