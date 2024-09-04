133cb25680d3b056218d85de2cca604904d9658a
Yashashvi Saini <yash03saini@gmail.com>
2024-09-04 19:46:41 +0530

    Update MyPostWidget.jsx

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -46,7 +46,7 @@ const MyPostWidget = ({ picturePath }) => {
       formData.append("picturePath", image.name);
     }
 
-    const response = await fetch(`http://complete-social-server.vercel.app/posts`, {
+    const response = await fetch(`https://complete-social-server.onrender.com/posts`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}` },
       body: formData,
