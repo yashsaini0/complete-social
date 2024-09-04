ccdcce23c781f6003de42be13ceed2187e0c08c8
Yashashvi Saini <yash03saini@gmail.com>
2024-09-04 19:47:42 +0530

    Update PostsWidget.jsx

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -9,7 +9,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
 
   const getPosts = async () => {
-    const response = await fetch("http://complete-social-server.vercel.app/posts", {
+    const response = await fetch("https://complete-social-server.onrender.com/posts", {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
@@ -19,7 +19,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
   const getUserPosts = async () => {
     const response = await fetch(
-      `http://complete-social-server.vercel.app/posts/${userId}/posts`,
+      `https://complete-social-server.onrender.com/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
