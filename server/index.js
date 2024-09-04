f5fadae55be8cd628e5d30d0c58d0192a0cf4153
Yashashvi Saini <yash03saini@gmail.com>
2024-09-04 19:26:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://complete-social-client.vercel.app", // Adjust this to your client’s URL
+  })
+);
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
 /* FILE STORAGE */
