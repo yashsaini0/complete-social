51e452fc059d7fb3d987200e9b458456600174eb
Yashashvi Saini <yash03saini@gmail.com>
2024-09-04 19:47:12 +0530

    Update PostWidget.jsx

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -35,7 +35,7 @@ const PostWidget = ({
   const primary = palette.primary.main;
 
   const patchLike = async () => {
-    const response = await fetch(`http://complete-social-server.vercel.app/posts/${postId}/like`, {
+    const response = await fetch(`https://complete-social-server.onrender.com/posts/${postId}/like`, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -64,7 +64,7 @@ const PostWidget = ({
           height="auto"
           alt="post"
           style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-          src={`http://complete-social-server.vercel.app/assets/${picturePath}`}
+          src={`https://complete-social-server.onrender.com/assets/${picturePath}`}
         />
       )}
       <FlexBetween mt="0.25rem">
