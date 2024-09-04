195e004b776c7bebaa2fc48e4a992e9cd23f63aa
Yashashvi Saini <yash03saini@gmail.com>
2024-09-04 19:43:17 +0530

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://complete-social-server.vercel.app/users/${_id}/${friendId}`,
+      `https://complete-social-server.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
